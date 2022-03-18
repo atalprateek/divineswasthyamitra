90bf4ae8b708999ad92595d587b3ae17dbdad25c
Atal Prateek Barla <46607024+atalprateek@users.noreply.github.com>
2022-03-18 12:00:10 +0530

    Message

--- application/config/autoload.php
+++ application/config/autoload.php
@@ -89,7 +89,7 @@ $autoload['drivers'] = array();
 |
 |	$autoload['helper'] = array('url', 'file');
 */
-$autoload['helper'] = array('url','file','form','menu','redirect','string','slug','upload','image','home');
+$autoload['helper'] = array('url','file','form','menu','redirect','string','slug','upload','image','home','message');
 
 /*
 | -------------------------------------------------------------------
