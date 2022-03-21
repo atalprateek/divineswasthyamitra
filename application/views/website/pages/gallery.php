bd326eb023ad3f838dbd616021d2221ea94bd287
Atal Prateek Barla <prateek.atal@gmail.com>
2022-03-21 15:23:28 +0530

    gallery

--- application/views/website/pages/gallery.php
+++ application/views/website/pages/gallery.php
@@ -6,6 +6,17 @@
                         <h2>Gallery</h2>
                         <hr style="border: 1px solid #e42121">
                         <br><br><br>
+                        <div class="row">
+                            <?php
+                                for($i=1;$i<=9;$i++){
+                            ?>
+                            <div class="col-md-3">
+                                <img src="<?= file_url('assets/images/gallery/image-'.$i.'.jpg'); ?>" alt="">
+                            </div>
+                            <?php
+                                }
+                            ?>
+                        </div>
                     </div>
                 </div>
             </div>
