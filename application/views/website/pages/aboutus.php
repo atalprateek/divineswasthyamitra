cdf73554cf74ceb0a8b6cebd326bebfe5dc281c4
Atal Prateek Barla <prateek.atal@gmail.com>
2022-03-23 13:56:42 +0530

    about us

--- application/views/website/pages/aboutus.php
+++ application/views/website/pages/aboutus.php
@@ -2,10 +2,15 @@
         <section class="affordable-homes" style="background:#ffffff;">
             <div class="container"> 
                 <div class="row">
-                    <div class="col-md-12">
+                    <div class="col-md-12 about-us">
                         <h2>About Us</h2>
                         <hr style="border: 1px solid #e42121">
-                        <br><br><br>
+                        <br>
+                        <p><strong>"Swathya Mitra"</strong> - A project of Divine Touch Medi Clinic, Agartala. In that project we are providing preventive health check up facilities to the beneficiaries of Tripura. We will be covering each &amp; every corner of Agartala covered under AMC ward through our Health Care Associates. Our U.S.P is each &amp; every month our trained registered Nursing team will visit &amp; examine the house of enrolled person(s) &amp; will check his/her essential parameters for health check up such as Blood Sugar, Blood Pressure, ECG, Pulse, SPO2, B.M.I, Serum Creatinine, Haemoglobin, Ear &amp; Eye Screening etc. Enrolled person will also get some extra benefits like discounts on Medicines, Other Investigations, Doctor Consultation, Home Collection etc. Prevention is better than Cure - Preventive health check up will help in early detection of illness &amp; can save lives from serious health issues. We will be doing Medical camps at various schools, colleges , Clubs, Societies, Govt. Offices &amp; Corporate Sectors in every month to create awareness of preventive health care.</p>
+                        <p><strong>What is Preventive Health Care?</strong> - Preventive Health Care tips include identification &amp; minimization of disease risk factors, existing disease course improvement, &amp; early disease direction through screening. This has become crucial since it allows people to know about their health issues well within time.</p>
+                        <p><strong>Mission:-</strong> To provide preventive health care facilities to the beneficiaries of Tripura covered under AMC ward.</p>
+                        <p><strong>Vision :-</strong> To make people free from serious health issues.</p>
+                        <br><br>
                     </div>
                 </div>
             </div>
